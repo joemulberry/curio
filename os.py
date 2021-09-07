a1b48450905216e9b478de7b1211b53df430618c
joemulberry <emailmeit@protonmail.com>
2021-09-07 14:44:14 +0100

    offsets to tokenids within the update call

--- os.py
+++ os.py
@@ -2,19 +2,14 @@ import requests
 import json
 
 def calc_eth(value, info):
-    value = int(value)
-    # dedecimaled = value ^^ int(info['decimals'])
-    return info
+    
+    return(info)
 
-url = "https://api.opensea.io/wyvern/v1/orders"
-querystring = {"asset_contract_address": "0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313", "bundled": "false", "include_bundled": "false",
-               "include_invalid": "false", "token_id": "20", "limit": "50", "offset": "0", "order_by": "created_date", "order_direction": "desc"}
-headers = {"Accept": "application/json"}
-response = requests.request("GET", url, headers=headers, params=querystring)
+url = 'https://github.com/joemulberry/curio/raw/main/orders.json'
+resp = requests.get(url)
+orders = json.loads(resp.text)
 
-orders = json.loads(response.text)['orders']
-
-print(orders[0])
+# print(orders[0])
 
 for order in orders:
     
@@ -23,10 +18,10 @@ for order in orders:
         'name': order['asset']['name'],
         'side': order['side'],
         'eth': calc_eth(order['base_price'], order['payment_token_contract'])
-        
+
         
     }
     
-    print(format(123000000000000, '.6f'))
-
-print(orders[0].keys())
+    if d['token_id'] != '20':
+        print(d)
+        # print(orders[0].keys())
--- os_pull.py
+++ os_pull.py
@@ -1,18 +1,17 @@
 import requests
 import json
 
-call_offsets = ['0', '50', '100', '150', '200']
+token_ids = [str(f) for f in range(1,30)]
 dicts = []
 
-for call_offset in call_offsets:
-
+for token_id in token_ids:
     url = "https://api.opensea.io/wyvern/v1/orders"
     querystring = {"asset_contract_address": "0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313", "bundled": "false", "include_bundled": "false",
-                   "include_invalid": "false", "limit": "50", "offset": call_offset, "order_by": "created_date", "order_direction": "desc"}
+                   "include_invalid": "false", "token_id": token_id, "limit": "50", "offset": "0", "order_by": "created_date", "order_direction": "desc"}
     headers = {"Accept": "application/json"}
     response = requests.request("GET", url, headers=headers, params=querystring)
-    orders = json.loads(response.text)['orders']
+    orders=json.loads(response.text)['orders']
     dicts += orders
-
+    
 with open('orders.json', 'w') as fout:
     json.dump(dicts, fout)
