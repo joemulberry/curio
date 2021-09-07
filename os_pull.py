e0ef22ac42b489dbb302fe0b9d0e682c8abdd347
joemulberry <emailmeit@protonmail.com>
2021-09-07 14:49:00 +0100

    switched to tokenid

--- os_pull.py
+++ os_pull.py
@@ -6,12 +6,12 @@ dicts = []
 
 for token_id in token_ids:
     url = "https://api.opensea.io/wyvern/v1/orders"
-    querystring = {"asset_contract_address": "0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313", "bundled": "false", "include_bundled": "false",
-                   "include_invalid": "false", "token_id": token_id, "limit": "50", "offset": "0", "order_by": "created_date", "order_direction": "desc"}
+    querystring = {"asset_contract_address": "0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313", "bundled": "false", "include_bundled": "false", "include_invalid": "false", "token_id": token_id, "limit": "50", "offset": "0", "order_by": "created_date", "order_direction": "desc"}
     headers = {"Accept": "application/json"}
     response = requests.request("GET", url, headers=headers, params=querystring)
     orders=json.loads(response.text)['orders']
     dicts += orders
-    
+    print(orders[0]['name'], 'added')
+
 with open('orders.json', 'w') as fout:
     json.dump(dicts, fout)
