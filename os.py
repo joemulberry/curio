355b7bbf28fa64dfe6e6b02ba2ad6e78b64430b8
joemulberry <emailmeit@protonmail.com>
2021-09-07 20:17:17 +0100

    new data and improved par

--- os.py
+++ os.py
@@ -11,17 +11,48 @@ orders = json.loads(resp.text)
 
 # print(orders[0])
 
-for order in orders:
-    
-    d = {
-        'token_id': order['asset']['token_id'],
-        'name': order['asset']['name'],
-        'side': order['side'],
-        'eth': calc_eth(order['base_price'], order['payment_token_contract'])
+orders_data = []
 
-        
-    }
+for order in orders:
+    token_id = order['asset']['token_id']
+    base_price = float(order['base_price'])
+    decimals = int(order['payment_token_contract']['decimals'])
+    eth_price = float(order['payment_token_contract']['eth_price'])
+    currency = order['payment_token_contract']['symbol']
+    quantity = int(order['quantity'])
+    side = order['side']
+
+    base_price = (base_price / (10 ** decimals)) / quantity
+    base_price = base_price * eth_price
+    # base_price = float(base_price[:len(base_price)-decimals] + '.' + base_price[len(base_price)-decimals:])
+    # base_price = (base_price * eth_price) / quantity
     
-    if d['token_id'] != '20':
-        print(d)
-        # print(orders[0].keys())
+    if base_price == 8000000000000000.0:
+        print("-"*40)
+        print(order)
+        print("-"*40)
+
+    if currency != "DAI":
+        if side == 0:
+            temp = {
+                'token_id': token_id, 
+                'value': base_price, 
+                'type': 'bid',
+                'currency': currency,
+                'ethprice': eth_price
+            }
+            orders_data.append(temp)
+        else:
+            temp = {
+                'token_id': token_id,
+                'value': base_price,
+                'type': 'ask',
+                'currency': currency,
+                'ethprice': eth_price
+            }
+            orders_data.append(temp)
+
+
+print(orders_data)
+
+print(orders[0]['target'])
